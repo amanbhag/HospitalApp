5410956a6b4033acc0ff81351ede268d129958b8
amanbhag <amanbhagtani@gmail.com>
2023-05-18 12:25:10 +0530

    added deployment

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -12,7 +12,9 @@ const PatientGrid = () => {
 
   const fetchPatients = async () => {
     try {
-      const response = await axios.get("http://localhost:4000/patients"); // Replace with your API endpoint
+      const response = await axios.get(
+        "https://hospitallapp.onrender.com/patients"
+      ); // Replace with your API endpoint
       setPatients(response.data);
       setActivePatient(response.data[0]);
       setBillDetails(response.data[0].billDetails);
